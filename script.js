405c4488a653e888f0d8007ba8d28043c162383d
Mateen Mirani <mateenmirani125@gmail.com>
2021-06-16 22:12:55 +0500

    add some new files

---
+++ script.js
@@ -0,0 +1,36 @@
+let navLinks = document.getElementById("navLinks");
+function showMenu() {
+  navLinks.style.right = "0";
+}
+
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
+
+function indoorDetail() {
+  location.href = "indoorDetail.html";
+}
+
+function outdoorDetail() {
+  location.href = "outDoorDetail.html";
+}
+
+function backToIndoor() {
+  location.href = "indoorCat.html";
+}
+
+function backToOutdoor() {
+  location.href = "outdoorCat.html";
+}
+
+function indoorCat() {
+  location.href = "indoorCat.html";
+}
+
+function outdoorCat() {
+  location.href = "outdoorCat.html";
+}
+
+function homePage() {
+  location.href = "all.html";
+}
